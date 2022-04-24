bfe535ae97ac0e76a72060fdab9a150cc7058697
realSixi <>
2022-04-24 16:04:46 +0200

    effect breath_center * cleanup code * add comments

--- src/effect_breath_center.h
+++ src/effect_breath_center.h
@@ -7,41 +7,27 @@ class EffectBreathCenter : public Effect
 {
 private:
 public:
-    static double ease(double t)
-    {
-        return 0.5 * (1 + sin(3.1415926 * (t - 0.5)));
-    }
     static void run(BeatInfo beatInfo, CRGBSet leds, int numLeds)
     {
-
+        // breath speed: every beat
         int frame = beatInfo.animationFrame(1);
 
-        // int byteFrame = map(frame, 0, 1000, 0, 255);
-
-        // float factor = ease(sin8(byteFrame) / 255.);
-
+        // turn of all leds
         leds = CRGB::Black;
 
         int byteFrame = map(frame, 0, 1000, 0, 255);
+
+        // width of the lightended leds, min 10% of numleds
         int width = map(sin8(byteFrame), 0, 255, numLeds * 0.1, numLeds);
 
-        // leds[width] = CRGB::Red;
-
+        // center position
         int ledCenter = numLeds / 2;
 
+        // rotate color; full rotation at 16 beats
         int color = map(beatInfo.animationFrame(16), 0, 1000, 0, 255);
 
+        // gradient from 10/255th to full to 10/255th for the calculated width, starting at center
         leds(ledCenter - width / 2, ledCenter + width / 2).fill_gradient(CHSV(color, 255, 10), CHSV(color, 255, 255), CHSV(color, 255, 10));
-
-        // for(int i = 0; i < numLeds; i++){
-        //     leds[i] = CHSV(0, 255, i * (width / 255.));
-        // }
-
-        // for (int i = 0; i < numLeds; i++)
-        // {
-        //     int value = cos8(map(i, -numLeds / 2, numLeds / 2, 64, 255));
-        //     leds[i] = CHSV(0, 255, value * factor);
-        // }
     }
 };
 
