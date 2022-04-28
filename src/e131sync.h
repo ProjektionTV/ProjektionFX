c4d58be3091e716f2557cc544419b3a13fc6f1b5
realSixi <77362594+realSixi@users.noreply.github.com>
2022-04-28 10:10:30 +0200

    wip revert changes to e131 channel-size revert sending/begin of packeges split leds > 170 into multiple universes

--- src/e131sync.h
+++ src/e131sync.h
@@ -9,6 +9,7 @@ class E131Sync
 private:
     uint16_t universeTX = 1;
     uint8_t DMXsequence = 1;
+    long previousMillis = 0;
 
 public:
     void setup()
@@ -16,17 +17,36 @@ public:
     }
     void loop()
     {
-        if (DMXsequence > 0xFF)
-            DMXsequence = 1;
-        e131.setSequenceNumber(DMXsequence++);
-        e131.setSourceName("ProjektionFX");
-
-        for (int i = 0; i < NUM_LEDS; i++)
+        // according to https://github.com/Aircoookie/WLED/issues/2417#issuecomment-995487832 
+        // and https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/lwip.html#limitations
+        // send might fail, if rate is to high;
+        // therefore -> limit to 60fps
+        if (millis() - previousMillis >= (1000/30))
         {
-            CRGB led = leds[i];
-            e131.setRGB(i * 3, led.r, led.g, led.b); // not sure about the order; seems strange, but fits for me.
+            previousMillis = millis();
+            if (DMXsequence > 0xFF)
+                DMXsequence = 1;
+            e131.setSequenceNumber(DMXsequence++);
+            e131.setSourceName("ProjektionFX");
+
+            int ledsLeft = NUM_LEDS;
+            int universesSent = 0;
+            int universe = universeTX;
+            while (ledsLeft > 0)
+            {
+                Serial.printf("Sending %d - start %d - universe %d\n", min(170, ledsLeft), universesSent * 170, universe);
+                for (int i = 0; i < min(170, ledsLeft); i++)
+                {
+                    CRGB led = leds[i + (universesSent * 170)];
+                    e131.setRGB(i * 3, led.r, led.g, led.b); // not sure about the order; seems strange, but fits for me.
+                }
+                ledsLeft -= min(170, ledsLeft);
+
+                e131.sendPacket(universe++);
+                universesSent++;
+            }
+            Serial.println("");
         }
-        e131.sendPacket(universeTX);
     }
 };
 
