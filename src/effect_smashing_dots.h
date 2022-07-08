718c01b4c93879d8c9044682b82685e38be8cf4a
chillimag <108898640+chillimag@users.noreply.github.com>
2022-07-08 21:33:45 +0200

    Add files via upload
    New effect "smashing_dots"

---
+++ src/effect_smashing_dots.h
@@ -0,0 +1,45 @@
+#include <effect.h>
+
+#if !defined(EFFECT_SMASHING_DOTS_H__)
+#define EFFECT_SMASHING_DOTS_H__
+
+class EffectSmashingDots : public Effect
+{
+private:
+public:
+    static void run(BeatInfo& beatInfo, CRGBSet leds, int numLeds)
+    {
+        // speed 2 = flash on every beat
+        int frame = beatInfo.animationFrame(2);
+
+        // turn off all leds
+        leds = CRGB::Black;
+
+        // width of the bars
+        int width = numLeds / 10;
+
+        // brightness of the dots
+        int ramp = map(frame < 500 ? 499 - frame : 999 - frame, 0, 499, 4, 16); // negative sawtooth for flashing
+        int brightness = ramp*ramp - 1; // square to fade quickly from 255 to 15
+
+        // colors
+        CHSV barColor = CHSV(0, 0, 32); // white
+        CHSV dotColor = CHSV(map(beatInfo.animationFrame(32), 0, 999, 0, 255), 255, brightness);
+
+        // draw the bars
+        int barAmplitude = width/2;
+        int barOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, -barAmplitude, barAmplitude); // up and down ramp
+        leds(numLeds*1/4 - width/2 + barOffset, numLeds*1/4 + width/2-1 + barOffset) = barColor; // bar1
+        leds(numLeds*3/4 - width/2 - barOffset, numLeds*3/4 + width/2-1 - barOffset) = barColor; // bar2
+
+        // draw the dots
+        int dotAmplitude = ((numLeds/2 - width - barAmplitude*2) / 2 - 1) / 2;
+        int dotOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, -dotAmplitude, dotAmplitude); // up and down ramp
+        leds[dotAmplitude - dotOffset] = dotColor; // dot1
+        leds[numLeds/2 - dotAmplitude - dotOffset - 1] = dotColor; // dot2
+        leds[numLeds/2 + dotAmplitude + dotOffset] = dotColor; // dot3
+        leds[numLeds - dotAmplitude + dotOffset - 1] = dotColor; // dot4
+    }
+};
+
+#endif // EFFECT_SMASHING_DOTS_H__
