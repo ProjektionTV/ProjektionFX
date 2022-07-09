385badfda1e8422bec416c9d99b1b44bff4c3b04
chillimag <108898640+chillimag@users.noreply.github.com>
2022-07-09 02:18:15 +0200

    Update effect_smashing_dots.h
    improved calculation of positions to fix blank spaces

--- src/effect_smashing_dots.h
+++ src/effect_smashing_dots.h
@@ -28,18 +28,18 @@ public:
         CHSV dotColor = CHSV((byteFrame + 128) % 256, 255, brightness);
 
         // draw the bars
-        int barAmplitude = width/2;
-        int barOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, -barAmplitude, barAmplitude); // up and down ramp
-        leds(numLeds*1/4 - width/2 + barOffset, numLeds*1/4 + width/2-1 + barOffset) = barColor; // bar1
-        leds(numLeds*3/4 - width/2 - barOffset, numLeds*3/4 + width/2-1 - barOffset) = barColor; // bar2
+        int barRange = width;
+        int barOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, 0, barRange); // up and down ramp
+        leds(numLeds*1/4 - barOffset, numLeds*1/4 + barRange - barOffset - 1) = barColor; // bar1
+        leds(numLeds*3/4 - barRange + barOffset, numLeds*3/4 + barOffset - 1) = barColor; // bar2
 
         // draw the dots
-        int dotAmplitude = ((numLeds/2 - width - barAmplitude*2) / 2 - 1) / 2;
-        int dotOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, -dotAmplitude, dotAmplitude); // up and down ramp
-        leds[dotAmplitude - dotOffset] = dotColor; // dot1
-        leds[numLeds/2 - dotAmplitude - dotOffset - 1] = dotColor; // dot2
-        leds[numLeds/2 + dotAmplitude + dotOffset] = dotColor; // dot3
-        leds[numLeds - dotAmplitude + dotOffset - 1] = dotColor; // dot4
+        int dotRange = numLeds/4 - width - 1;
+        int dotOffset = map(frame < 500 ? frame : 999 - frame, 0, 499, 0, dotRange); // up and down ramp
+        leds[dotOffset] = dotColor; // dot1
+        leds[numLeds/2 - dotRange + dotOffset - 1] = dotColor; // dot2
+        leds[numLeds/2 + dotRange - dotOffset] = dotColor; // dot3
+        leds[numLeds - dotOffset - 1] = dotColor; // dot4
     }
 };
 
