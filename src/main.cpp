b9b1ed2ad05feccbe26a3fc2cbb937106ca73721
NoX <86572462+NoX209@users.noreply.github.com>
2022-11-18 22:33:46 +0100

    add https server with twitch player callback to /delay?latency=...

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,13 @@
 
 #include "e131sync.h"
 
+#include <HttpsWebServer.h>
+
 CRGBArray<NUM_LEDS> leds;
 
 BeatInfo beatInfo;
+
+HttpsWebServer https;
 
 void setup()
 {
@@ -36,6 +40,8 @@ void setup()
   FastLED.setMaxPowerInVoltsAndMilliamps(5, LED_MAX_MILLIAMP);
 
   webServer.begin();
+  https.generateSSLCert();
+  https.start();
 
 #ifdef ARTNET_ENABLED
   e131sync.setup();
@@ -50,6 +56,8 @@ void loop()
   ArduinoOTA.handle();
 
   beatInfo.loop();
+
+  https.loop();
 
   //effectsRunner.run();
 
